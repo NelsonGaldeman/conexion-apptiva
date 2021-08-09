32c3bd12c4a1c4b1bc9d6fe476cd1787cc4fc983
Nelson Galdeman <nelsongaldeman@gmail.com>
2021-08-09 19:36:49 +0100

    more social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import twitter from '../assets/images/social/twitter.png';
 import facebook from '../assets/images/social/facebook.png';
 import instagram from '../assets/images/social/instagram.png';
+import youtube from '../assets/images/social/youtube.png';
+import linkedin from '../assets/images/social/linkedin.png';
+import tiktok from '../assets/images/social/tiktok.png';
 
 class Social extends React.Component {
 
@@ -12,6 +15,9 @@ class Social extends React.Component {
                 {/* <a href="https://twitter.com/apptivalo" target="_blank" rel="noreferrer"><img className={this.props.classes} src={twitter} alt="Twitter"/></a> */}
                 <a href="https://www.facebook.com/Apptivalo-476624526218839/" target="_blank" rel="noreferrer"><img className={this.props.classes} src={facebook} alt="Facebook"/></a>
                 <a href="https://www.instagram.com/apptivalo.app" target="_blank" rel="noreferrer"><img className={this.props.classes} src={instagram} alt="Instagram"/></a>
+                <a href="https://youtube.com/channel/UCpn2u4AE55IK-xLiThAlBBw" target="_blank" rel="noreferrer"><img className={this.props.classes} src={youtube} alt="Youtube"/></a>
+                <a href="https://www.linkedin.com/company/apptivalo" target="_blank" rel="noreferrer"><img className={this.props.classes} src={linkedin} alt="Linkedin"/></a>
+                <a href="https://vm.tiktok.com/ZMdEuA7Yv/" target="_blank" rel="noreferrer"><img className={this.props.classes} src={tiktok} alt="TikTok"/></a>
             </div>
         )
     }
